00ea4933c0a29c560684433b36932bfd71be6437
kalvin.alexander <scempipelines@034gc.onmicrosoft.com>
2021-11-26 14:58:00 +0000

    Modified Event Details to match OPP

--- SCEM/Solution/WebResources/tc_/OPP/SetEventDetailsType.js
+++ SCEM/Solution/WebResources/tc_/OPP/SetEventDetailsType.js
@@ -3,6 +3,7 @@
 function setEventDetailType() {
 	debugger;
 	var globalContext = Xrm.Utility.getGlobalContext();
+	var selection = Xrm.Page.getAttribute("tc_eventdetailtypecd").getValue();
 	var appName = globalContext.getCurrentAppName()
 		.then(function (appName) {
 			var fieldName = "tc_eventdetailtypecd";
@@ -18,12 +19,19 @@ function setEventDetailType() {
 			}
 			else if (appName === "SCEM" || appName === "GMCI") {
 				Xrm.Page.getAttribute(fieldName).setValue(3);
-				Xrm.Page.getAttribute(fieldName).fireOnChange();
 				Xrm.Page.getControl(fieldName).setDisabled(true);
 				Xrm.Page.getControl("tc_actionnumber").setVisible(true);
 				Xrm.Page.getControl("tc_opi").setVisible(true);
 				Xrm.Page.getControl("tc_coordinator").setVisible(true);
 			}
+			else if (appName === "ASIES" || appName === "SMISA"){
+				if (selection == 3){
+					Xrm.Page.getControl("tc_commitmentid").setVisible(true);
+				}else{
+					Xrm.Page.getControl("tc_commitmentid").setVisible(false);
+				}
+				Xrm.Page.getControl(fieldName).removeOption(4);//Issue
+			}					
 			else {
 				Xrm.Page.getControl(fieldName).removeOption(4);//Issue
 			}
